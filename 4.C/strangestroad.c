0059779f976a4e903f5bc29b522620f064797569
Ashwini Patel <55460172+strangestroad@users.noreply.github.com>
2020-09-25 16:18:44 +0530

    Create strangestroad.c

---
+++ 4.C/strangestroad.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+ int main()
+  {
+     printf("Hello!\n");
+     printf("Name   : Ashwini Patel\n");
+     printf("Age    : 20\n");
+     printf("Username : strangestroad\n");
+     return(0);
+  }
