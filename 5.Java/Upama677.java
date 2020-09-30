631345e99d8b8d8b781062fa82ad886849aa4c76
Upama dutta <65466052+Upama677@users.noreply.github.com>
2020-09-30 23:27:50 +0530

    Create Upama677.java

---
+++ 5.Java/Upama677.java
@@ -0,0 +1,10 @@
+class Upama677 {
+    public static void main(String[] args) {
+
+    String name = "Upama Dutta";
+    int age = 20yrs ;
+    String username = "Upama677";
+
+        System.out.println("Hello!\nName: "+name+"\nAge: "+age+ "\nUsername: "+username);
+    }
+}
