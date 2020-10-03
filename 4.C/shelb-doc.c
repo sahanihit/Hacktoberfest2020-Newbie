06b35a5cc2f8ea926efac364dd73dbd6644f3875
Shelby Cignetti <shelby.cignetti@gmail.com>
2020-10-02 22:40:12 -0400

    made my first c program

--- 4.C/shelb-doc.c
+++ 4.C/shelb-doc.c
@@ -1,9 +1,9 @@
 #include <stdio.h> 
 int main()
 {
-   printf("Hello!\n");
-     printf("Name   : *YOUR NAME*\n");
+printf("Hello!\n");
+    printf("Name   : Shelby De Oliveira\n");
      printf("Age    : *YOUR AGE*\n");
      printf("Username : *YOUR USERNAME*\n");
-   return(0);
+return(0);
 }
