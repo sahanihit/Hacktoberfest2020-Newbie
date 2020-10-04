84db50078d98d2cc7eb8cc2e714e2d7fed970a37
akumar0709 <67645056+akumar0709@users.noreply.github.com>
2020-10-04 17:10:53 +0530

    Update and rename #TEMPLATE.py to akumar0709.py

--- 3.Python/akumar0709.py
+++ 3.Python/akumar0709.py
@@ -1,4 +1,4 @@
-name, age = "*YOUR NAME*", *YOUR AGE*
-username = "*YOUR USERNAME*"
+name, age = "Ashwani", *21*
+username = "akumar0709"
 print ('Hello!')
 print("Name: {}\nAge: {}\nUsername: {}".format(name, age, username))
