6b111c723bcdfb30552f0c5ededfe86293aae1cc
akumar0709 <67645056+akumar0709@users.noreply.github.com>
2020-10-04 17:12:42 +0530

    Update and rename #TEMPLATE.c to akumar0709.c

---
+++ 4.C/akumar0709.c
@@ -0,0 +1,9 @@
+#include <stdio.h> 
+ int main()
+  {
+     printf("Hello!\n");
+     printf("Name   : Ashwani\n");
+     printf("Age    : 21\n");
+     printf("Username : akumar0709\n");
+     return(0);
+  }
