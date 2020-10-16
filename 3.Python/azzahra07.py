992b4e90ab8eb2406bad365bc43d682629a0e006
azzahra07 <zzhrslsbl@gmail.com>
2020-10-16 09:13:05 +0700

    add my contrib

---
+++ 3.Python/azzahra07.py
@@ -0,0 +1,4 @@
+name, age = "Salsabila Azzahra", 21
+username = "azzahra07"
+print ('Hello!')
+print("Name: {}\nAge: {}\nUsername: {}".format(name, age, username))
---
+++ 4.C/azzahra07.c
@@ -0,0 +1,9 @@
+#include <stdio.h> 
+ int main()
+  {
+     printf("Hello!\n");
+     printf("Name   : Salsabila Azzahra\n");
+     printf("Age    : 21\n");
+     printf("Username : azzahra07\n");
+     return(0);
+  }
