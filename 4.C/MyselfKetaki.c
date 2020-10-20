732128fe941619149cad454140741dc2d608d71d
Ketaki Gangadhar <71887363+Ketaki-Gangadhar@users.noreply.github.com>
2020-10-20 23:52:39 +0530

    Create MyselfKetaki.c

---
+++ 4.C/MyselfKetaki.c
@@ -0,0 +1,11 @@
+// C program
+
+#include<studio.h>
+int main()
+{
+printf("Hello!\n");
+printf("Name : Ketaki \n"); 
+printf("Quote : Keep Moving \n")
+printf("Username : Ketaki-Gangadhar\n");
+return (0);
+}
