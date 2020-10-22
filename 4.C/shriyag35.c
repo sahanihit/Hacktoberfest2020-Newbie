1e54e03682750f304f49cdcd71edf4551b75b42c
shriyag35 <72926009+shriyag35@users.noreply.github.com>
2020-10-22 09:06:41 -0400

    Create shriyag35.c

---
+++ 4.C/shriyag35.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+ int main()
+  {
+     printf("Hello!\n");
+     printf("Name: Shriya\n");
+     printf("Age: 35\n");
+     printf("Username: shriyag35\n");
+     return(0);
+  }
