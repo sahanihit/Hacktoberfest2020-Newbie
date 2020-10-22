60ea29dde078eaa9fa257e34e25fac52124160e8
BaguetteDev <72872790+BaguetteDev@users.noreply.github.com>
2020-10-22 09:44:44 -0400

    Create BaguetteDev.java

---
+++ 5.Java/BaguetteDev.java
@@ -0,0 +1,10 @@
+class baguetteDev {
+    public static void main(String[] args) {
+
+    String name = "Will Martin";
+    int age = 16 ;
+    String username = "BaguetteDev";
+
+        System.out.println("Hello!\nName: "+name+"\nAge: "+age+ "\nUsername: "+username);
+    }
+}
