85c727e6e0d41bb62a02ea56d08a13408e248f14
bilialv170 <72928619+bilialv170@users.noreply.github.com>
2020-10-22 10:19:09 -0400

    Create bilialv170.java

---
+++ bilialv170.java
@@ -0,0 +1,16 @@
+
+
+class bilialv170 {
+    public static void main(String[] args) {
+
+    String name = "Bili Alvarado";
+    int age = 17 ;
+    String username = "bilialv170";
+
+        System.out.println("Hello!\nName: "+name+"\nAge: "+age+ "\nUsername: "+username);
+    }
+}
+
+
+
+
