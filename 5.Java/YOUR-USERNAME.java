baee5f757406ce92f421bebf240873ed94bfcc45
AsiCas1417 <72926080+AsiCas1417@users.noreply.github.com>
2020-10-22 08:29:27 -0400

    Create YOUR-USERNAME.java

---
+++ 5.Java/YOUR-USERNAME.java
@@ -0,0 +1,10 @@
+class AsiCas1417 {
+    public static void main(String[] args) {
+
+    String name = "Ariel Castle";
+    int age = 21 ;
+    String username = "AsiCas1417";
+
+        System.out.println("Hello!\nName: "+name+"\nAge: "+age+ "\nUsername: "+username);
+    }
+}
