970d266fe1008b5a7a30d86ba2ac2490366415cc
saclig202 <72928504+saclig202@users.noreply.github.com>
2020-10-26 10:17:43 -0400

    Create saclig202.c

---
+++ 4.C/saclig202.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+ int main()
+  {
+     printf("Hello!\n");
+     printf("Name   : Sacha lignon\n");
+     printf("Age    : 18\n");
+     printf("Username : saclig202\n");
+     return(0);
+  }
