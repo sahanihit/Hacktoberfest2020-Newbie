ddf8cde411d41bfea4cf3cdaca16f306216530f3
Vedant Darji <52580404+vedant1707@users.noreply.github.com>
2020-10-29 15:24:47 +0530

    Create Vedant_Darji_HEllo.C

---
+++ 4.C/Vedant_Darji_HEllo.C
@@ -0,0 +1,9 @@
+#include <stdio.h>
+ int main()
+  {
+     printf("Hello!\n");
+     printf("Name   : Vedant Darji\n");
+     printf("Country   : India\n");
+     printf("Github Username : vedant1707\n");
+     return(0);
+  }
