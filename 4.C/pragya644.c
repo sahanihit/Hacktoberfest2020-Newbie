b6bd17566e5cf8a861febd7648a3a1ad178cf034
pragya644 <singhalpragya3@gmail.com>
2021-10-04 13:37:42 +0530

    Profile in C

---
+++ 4.C/pragya644.c
@@ -0,0 +1,9 @@
+#include <stdio.h> 
+ int main()
+  {
+     printf("Hello Everyone!\n");
+     printf("Name   : Pragya Singhal\n");
+     printf("Age    : 18\n");
+     printf("Username : pragya644\n");
+     return(0);
+  }
