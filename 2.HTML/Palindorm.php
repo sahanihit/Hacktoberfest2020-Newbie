9c674bb9a24962fdd83bec1796b1791421d96d76
jaybhatia55 <45117917+jaybhatia55@users.noreply.github.com>
2021-10-05 23:05:56 +0530

    program to check if number is palindrome or not
    php program to check if number is palindrome or not

---
+++ 2.HTML/Palindorm.php
@@ -0,0 +1,20 @@
+<form method="post">        
+Enter a Number: <input type="text" name="num"/><br>  
+<button type="submit">Check</button>  
+</form>  
+<?php   
+    if($_POST)  
+    {  
+        //get the value from form  
+        $num = $_POST['num'];  
+        //reversing the number  
+        $reverse = strrev($num);  
+          
+        //checking if the number and reverse is equal  
+        if($num == $reverse){  
+            echo "The number $num is Palindrome";     
+        }else{  
+            echo "The number $num is not a Palindrome";   
+        }  
+}     
+      ?>  
